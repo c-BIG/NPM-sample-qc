6c802d6b0a52aea9c26df06b9fa355bb535c51b8
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-02-28 18:00:24 +0800

    fix broken image in github pages

--- docsrc/build.sh
+++ docsrc/build.sh
@@ -10,6 +10,9 @@ sphinx-apidoc --output-dir api --no-toc ../bin/metrics
 # create sphinx docs
 make github
 
+# copy images
+cp ../npm-sample-qc-overview.PNG ../docs/
+
 # clean up docsrc
 [ -d api ] && rm -r api
 [ -d build ] && rm -r build
