dd5d3ad7512f3beb9bad3a03f34a548c946a8439
Ubuntu <ubuntu@ip-172-31-10-126.ap-southeast-1.compute.internal>
2022-05-23 06:08:31 +0000

    nextflow job submission

--- tests/run.sh
+++ tests/run.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 
-# check if nextflow is available
-which nextflow >/dev/null || { echo "ERROR: nextflow is not available"; exit 1; }
+export NXF_DEFAULT_DSL=1
+export CAPSULE_LOG=none
 
-# make sure we are in the test directory
-ls ../main.nf ../tests  >& /dev/null || { echo "ERROR: must run from NPM-sample-qc/tests directory"; exit 1; }
+uid=`uuidgen`
 
-# define directories
-projectdir=$(realpath "$(pwd)/..")
-outdir="$1"
-if [ -z "$outdir" ]; then
-    echo "ERROR: Missing outdir argument, e.g.:" 1>&2
-    echo "  outdir='/data/13000026/pipeline/dev/NPM-sample-qc-aux/tests/<my_run>'" 1>&2
-    echo "  ./run.sh \$outdir"
-    exit 1
-fi
-workdir="${outdir}/work"
-
-# define profile
-profile="$2"
-if [ -z "$profile" ]; then
-    profile="nscc"
-fi
-echo "Running with profile \"${profile}\"."
-echo "This option can be changed when calling the script:"
-echo "  ./run.sh <outdir> <profile>"
-
-# run nextflow
-nextflow run ${projectdir}/main.nf \
-    -config ${projectdir}/conf/nextflow.config \
-    -params-file $(pwd)/sample_params.yml \
-    -profile ${profile} \
-    -resume \
-    -work-dir ${workdir} \
-    --outdir ${outdir} \
-    --keep_workdir
+nextflow run NPM-sample-qc/main.nf \
+	-config NPM-sample-qc/conf/nextflow.config \
+	-params-file sample_params.yml \
+	-resume \
+	--outdir s3://npm-ica-staging-sse/ica-byob/npm-ica-staging-sse/qc-test/${uid}/output \
+	-profile local \
+	-bg >nf-test.${uid}.log
