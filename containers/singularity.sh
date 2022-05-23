8eb3642a8863b22a9e19b9b2965282593e53f1d3
Ubuntu <ubuntu@ip-172-31-10-126.ap-southeast-1.compute.internal>
2022-05-23 02:27:13 +0000

    update with latest build

--- containers/singularity.sh
+++ containers/singularity.sh
@@ -6,3 +6,5 @@ then
     sudo singularity build --force npm-sample-qc.simg singularity.def
     singularity exec npm-sample-qc.simg echo "Build complete"
 fi
+### cp npm-sample-qc.simg npm-sample-qc-20052022.simg
+### aws s3 cp npm-sample-qc-20052022.simg s3://npm-sample-qc/
