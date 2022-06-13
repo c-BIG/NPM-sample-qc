b765d5e012db44c78fb1a686eeac76660fe807b9
Ubuntu <ubuntu@ip-172-31-10-126.ap-southeast-1.compute.internal>
2022-06-13 00:03:04 +0000

    nextflow job submission script

--- tests/run.sh
+++ tests/run.sh
@@ -2,13 +2,16 @@
 
 export NXF_DEFAULT_DSL=1
 export CAPSULE_LOG=none
+NXF_ANSI_LOG=false
 
 uid=`uuidgen`
+sampleid="NA12878"
 
 nextflow run NPM-sample-qc/main.nf \
-	-config NPM-sample-qc/conf/nextflow.config \
-	-params-file sample_params.yml \
+	-config NPM-sample-qc/conf/nextflow-docker.config \
+	-params-file NPM-sample-qc/tests/sample_params.aws.bam.yml \
 	-resume \
-	--outdir s3://npm-ica-staging-sse/ica-byob/npm-ica-staging-sse/qc-test/${uid}/output \
-	-profile local \
+	--outdir s3://npm-grids/gnanakkan/npm-sample-qc/output/${sampleid}-bam-awsbatch \
+	-bucket-dir s3://npm-grids/gnanakkan/npm-sample-qc/output/${sampleid}-bam-awsbatch-work \
+	-profile awsbatch \
 	-bg >nf-test.${uid}.log
