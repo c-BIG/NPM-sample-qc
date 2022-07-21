a079b18004c03f22e81bd2e589fa12fbd3f80f5b
Ubuntu <ubuntu@ip-172-31-10-126.ap-southeast-1.compute.internal>
2022-07-21 06:54:42 +0000

    removed the absolute path PR #17

--- tests/run.sh
+++ tests/run.sh
@@ -12,8 +12,8 @@ nextflow run NPM-sample-qc/main.nf \
 	-config NPM-sample-qc/conf/nextflow-docker.config \
 	-params-file NPM-sample-qc/tests/sample_params.aws.bam.yml \
 	-resume \
-	--outdir output/${sampleid} \
-	-bucket-dir output/${sampleid}-work \
+	--outdir ${sampleid} \
+	-bucket-dir ${sampleid}-work \
 	-profile awsbatch \
 	-with-trace \
 	-bg >nf-test.${uid}.log
