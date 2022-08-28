c96627ba171dd6d07c5171f8e515d343cbfc4fe0
Ubuntu <ubuntu@ip-172-31-10-126.ap-southeast-1.compute.internal>
2022-08-28 16:43:33 +0000

    mionor update

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+REPO_PATH="NPM-sample-qc"
 
 export NXF_DEFAULT_DSL=1
 export CAPSULE_LOG=none
@@ -7,12 +8,11 @@ NXF_ANSI_LOG=false
 uid=`uuidgen`
 sampleid="NA12878"
 
-nextflow run NPM-sample-qc/main.nf \
-	-config NPM-sample-qc/nextflow.config \
-	-params-file NPM-sample-qc/tests/sample_params.aws.bam.yml \
+nextflow run $REPO_PATH/main.nf \
+	-config $REPO_PATH/nextflow.config \
+	-params-file $REPO_PATH/tests/sample_params.aws.bam.yml \
 	-resume \
-	--outdir ${sampleid} \
-	-bucket-dir ${sampleid}-work \
+	--outdir s3://npm-grids/${sampleid} \
+	-bucket-dir s3://npm-grids/${sampleid}-work \
 	-profile awsbatch \
-	-with-trace \
 	-bg >nf-test.${uid}.log
