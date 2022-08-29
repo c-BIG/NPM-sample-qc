97af1c785f93fee2f51448ccb4d2d166d3b8ade7
Ubuntu <ubuntu@ip-172-31-10-126.ap-southeast-1.compute.internal>
2022-08-29 01:04:09 +0000

    update on proccessing autosomes and its N regions

--- bin/compile_metrics.py
+++ bin/compile_metrics.py
@@ -67,7 +67,7 @@ def calculate_metrics(mqc):
 
 def save_output(d, outfile):
     with open(outfile, "w") as f:
-        d = {"biosample" : {"id" : args.sample_id}, "workflow_version" : {"version" : args.version}, "wgs_qc_metrics": d}
+        d = {"biosample" : {"id" : args.sample_id}, "workflow_version" : {"version" : args.version}, "wgs_qc_metrics" : d}
         json.dump(d, f, sort_keys=True, indent=4)
         f.write("\n")
 
