3749246255ba399868c85de80cb733e0f145d3bf
justinjj24 <ubuntu@ip-172-31-10-126.ap-southeast-1.compute.internal>
2022-08-31 05:21:06 +0000

    clean up test folder

--- tests/run.sh
+++ tests/run.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-REPO_PATH="NPM-sample-qc"
+cd test
 
 export NXF_DEFAULT_DSL=1
 export CAPSULE_LOG=none
 NXF_ANSI_LOG=false
 
-uid=`uuidgen`
-sampleid="NA12878"
-
-nextflow run $REPO_PATH/main.nf \
-	-config $REPO_PATH/nextflow.config \
-	-params-file $REPO_PATH/tests/sample_params.aws.bam.yml \
+nextflow run ../main.nf \
+	-config ../nextflow.config \
+	-params-file sample_params.yml \
 	-resume \
-	--outdir s3://npm-grids/${sampleid} \
-	-bucket-dir s3://npm-grids/${sampleid}-work \
-	-profile awsbatch \
-	-bg >nf-test.${uid}.log
+	-work-dir ./work \
+	--outdir ./ \
+	-profile docker
