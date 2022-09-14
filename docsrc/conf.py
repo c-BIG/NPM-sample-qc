e09f5e460b75209a189824f44328052b917c2677
Maxime HEBRARD <maxime.hebrard@gmail.com>
2022-09-14 10:14:05 +0800

    Fix Sphinx documentation

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -14,25 +14,21 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../bin'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'NPM-sample-qc'
 copyright = '2020, GIS'
 author = 'GIS'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
 extensions = [
     'sphinx_automodapi.automodapi',
     'sphinx_rtd_theme'
 ]
-numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
@@ -42,21 +38,20 @@ numpydoc_show_class_members = False
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
 # from https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+# html_context = {
+#     'css_files': [
+#         '_static/theme_overrides.css',  # override wide tables in RTD theme
+#         ],
+#      }
--- docsrc/index.rst
+++ docsrc/index.rst
@@ -1,12 +1,10 @@
 .. include:: ../README.rst
 
-
 .. toctree::
    :maxdepth: 1
    :caption: Metric definitions
 
    metrics
-
 
 .. # high-level package docs
    # triggers a warning if not sourced, here commented on purpose for simpler docs
