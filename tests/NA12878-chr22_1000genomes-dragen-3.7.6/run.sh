e1e456d2d71512b365be6fb03479a8d3301fa1cf
justinjj24 <gnanakkan@gis.a-star.edu.sg>
2022-10-03 02:19:19 +0000

    Create separate output folder for NA12878 whole genome and NA12878 chr22 only

---
+++ tests/NA12878-chr22_1000genomes-dragen-3.7.6/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CAPSULE_LOG=none
+NXF_ANSI_LOG=false
+
+nextflow run ../main.nf \
+	-config ../nextflow.config \
+	-params-file sample_params.yml \
+	-resume \
+	-work-dir ./work \
+	--outdir . \
+	-profile docker \
+	-bg >run.log
