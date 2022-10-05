302ff4720716c1c28a0c715b459006b1d119d01b
justinjj24 <gnanakkan@gis.a-star.edu.sg>
2022-10-05 02:12:23 +0000

    comments and run.sh path update

--- tests/NA12878-chr22_1000genomes-dragen-3.7.6/run.sh
+++ tests/NA12878-chr22_1000genomes-dragen-3.7.6/run.sh
@@ -3,8 +3,8 @@
 export CAPSULE_LOG=none
 NXF_ANSI_LOG=false
 
-nextflow run ../main.nf \
-	-config ../nextflow.config \
+nextflow run ../../main.nf \
+	-config ../../nextflow.config \
 	-params-file params.yml \
 	-resume \
 	-work-dir ./work \
