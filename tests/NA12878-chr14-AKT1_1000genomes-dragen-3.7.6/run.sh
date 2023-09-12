aab17fb87154d9bac81c2e4b7fd5402ff50d391a
justinjj24 <gnanakkan@gis.a-star.edu.sg>
2023-09-12 09:04:18 +0800

    revert to nextflow exe withut version path

--- tests/NA12878-chr14-AKT1_1000genomes-dragen-3.7.6/run.sh
+++ tests/NA12878-chr14-AKT1_1000genomes-dragen-3.7.6/run.sh
@@ -6,7 +6,7 @@
 export CAPSULE_LOG=none
 NXF_ANSI_LOG=false
 
-/Users/gnanakkan/bin/nf-23.09.1-edge/nextflow run ../../main.nf \
+nextflow run ../../main.nf \
 	-config ../../nextflow.config -config ./NA12878-chr14-AKT1.config \
         --inputs_list inputs.yaml \
 	-work-dir ./work \
