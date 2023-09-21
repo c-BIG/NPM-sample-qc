7ce1ca90637721353ffc3b24f6b5f268f99ee854
justinjj24 <gnanakkan@gis.a-star.edu.sg>
2023-09-21 13:15:27 +0800

    update verifybamid error strategy

--- tests/NA12878-chr14-AKT1_1000genomes-dragen-3.7.6/run.sh
+++ tests/NA12878-chr14-AKT1_1000genomes-dragen-3.7.6/run.sh
@@ -7,7 +7,7 @@ export CAPSULE_LOG=none
 NXF_ANSI_LOG=false
 
 nextflow run ../../main.nf \
-	-config ../../nextflow.config -config ./NA12878-chr14-AKT1.config \
+	-config ../../nextflow.config \
         --inputs_list params.yaml \
 	-work-dir ./work \
 	--publish_dir ./output
