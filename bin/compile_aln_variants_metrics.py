4cce7c309bcc2ee2e48d2054b3e8bf2aed6a50c9
justinjj24 <gnanakkan@gis.a-star.edu.sg>
2024-09-03 04:55:40 +0000

    code upde count_aln and compile_aln_variants

---
+++ bin/compile_aln_variants_metrics.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import pprint as pp
+import subprocess
+import numpy as np
+import sys
+import os
+from pathlib import Path
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sample_id", dest="sample_id", required=True,
+                        default=None,
+                        help="Sample ID")
+    parser.add_argument("--input_aln_metrics", dest="input_aln_metrics", required=True,
+                        default=None,
+                        help="Path to input aln metrics list")
+    parser.add_argument("--input_variants_metrics", dest="input_variants_metrics", required=True,
+                        default=None,
+                        help="Path to input variants metrics list")
+    parser.add_argument("--output_json", dest="output_json", required=False,
+                        default="./metrics.json",
+                        help="Path to output file for variant metrics. Default: ./variant_counts.json")
+    parser.add_argument("--scratch_dir", dest="scratch_dir", required=False,
+                        default="./",
+                        help="Path to scratch dir. Default: ./")
+    args = parser.parse_args()
+
+    # create scratch dir if it doesn't exist
+    Path(args.scratch_dir).mkdir(parents=True, exist_ok=True)
+
+    return args
+
+
+#with open("variant_counts.json", 'r') as f1:
+#    data1 = json.load(f1)
+
+#with open("NA12878-chr14-AKT1.variant.metrics.json", 'r') as f2:
+#    data2 = json.load(f2)
+
+#with open("merged_output_json", "w") as f:
+#    json.dump({"biosample": data1["biosample"], "wgs_qc_metrics": {**data1["wgs_qc_metrics"], **data2["wgs_qc_metrics"]}}, f, sort_keys=True, indent=4)
+#    f.write("\n")
+
+def data1(input_aln_metrics):
+    aln_input = {}
+    with open(input_aln_metrics, 'r') as f1:
+        aln_input = json.load(f1)
+    return aln_input
+
+def data2(input_variants_metrics):
+    variants_input = {}
+    with open(input_variants_metrics, 'r') as f2:
+        variants_input = json.load(f2)
+    return variants_input
+
+def save_output(data1, data2, outfile):
+    with open(outfile, "w") as f:
+        json.dump({"biosample": data1["biosample"], "wgs_qc_metrics": {**data1["wgs_qc_metrics"], **data2["wgs_qc_metrics"]}}, f, sort_keys=True, indent=4)
+        f.write("\n")
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    aln = data1(args.input_aln_metrics)
+    variants = data2(args.input_variants_metrics)
+    save_output(aln, variants, args.output_json)
